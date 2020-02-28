5f379aa9272cdaa9ff38cfb1b2345b34abf864b2
fcortesj <fcortesj@eafit.edu.co>
2020-02-28 17:52:55 -0500

    Adding Watch and Category Models
    For Watch model adding: Watch.php, watch migration, watch factory and table seeder.
    For Category model adding: Category.php, category migration, category factory and table seeder.

---
+++ app/Category.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Watch;
+
+/*
+    Category Model Class
+    Attributes : id, name, image, description
+*/
+
+class Category extends Model
+{
+    private const NAME = 'name';
+    private const IMAGE = 'image';
+    private const DESCRIPTION = 'description';
+    
+    protected $fillable = [Category::NAME,Category::IMAGE,Category::DESCRIPTION];
+
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id)
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getName()
+    {
+        return $this->attributes[Category::NAME];
+    }
+
+    public function setName($name)
+    {
+        $this->attributes[Category::NAME] = $name;
+    }
+
+    public function getImage()
+    {
+        return $this->attributes[Category::IMAGE];
+    }
+
+    public function setImage($image)
+    {
+        $this->attributes[Category::IMAGE] = $image;
+    }
+
+    public function getDescription()
+    {
+        return $this->attributes[Category::DESCRIPTION];
+    }
+
+    public function setDescription($description)
+    {
+        $this->attributes[Category::DESCRIPTION] = $description;
+    }
+
+    public function watches()
+    {
+        return $this->hasMany(Watch::class);
+    }
+}
---
+++ database/migrations/2020_02_28_000000_create_watches_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->integer('quantity');
+            $table->text('color');
+            $table->text('brand');
+            $table->text('reference');
+            $table->text('gender');
+            $table->integer('price');
+            $table->text('image');
+            $table->text('description');
+            $table->foreign('id_category')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watches');
+    }
+}
