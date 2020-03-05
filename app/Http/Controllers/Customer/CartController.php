5d25efacbb45bdbd3bb9288150dbb8a1729056f7
Santiago Soto <ssotom@eafit.edu.co>
2020-03-05 14:23:55 -0500

    added checkout view

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -9,12 +9,21 @@ use App\Watch;
 class CartController extends Controller
 {
 
-    public function index() {
+    public function index()
+    {
         $data = [];
         $data["title"] = "My Cart";
 
         return view('customer.cart.index')->with("data", $data);
     }
+
+    public function checkout()
+    {
+        $data = [];
+        $data["title"] = "Checkout";
+
+        return view('customer.cart.checkout')->with("data", $data);
+    } 
 }
 
 ?>
