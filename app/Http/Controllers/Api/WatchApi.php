90301b9e53ecac3d175e2496cdce8f719358f727
Rafael <rvillegasm@eafit.edu.co>
2020-05-06 14:17:00 -0500

    Adding the exposable api related to sport watches

---
+++ app/Http/Controllers/Api/WatchApi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Category;
+use App\Watch;
+
+class WatchApi extends Controller
+{
+    public function showSport()
+    {
+        $sportCategory = Category::where('name', 'K8gA')->first();
+        if (!$sportCategory) {
+            return null;
+        }
+        $sportWatches = Watch::where('category_id', $sportCategory->getId())->get();
+        foreach ($sportWatches as $watch) {
+            $watch->setImage($watch->getImage());
+        }
+
+        return $sportWatches;
+    }
+}
+
+?>
